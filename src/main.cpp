a3a86d849a69ed9f9fa0e627dc1962d4e7f4bab4
Hao  Xu <haoxugatech@gmail.com>
2017-07-18 11:04:42 -0700

    Add taskManager class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "mpi.h"
 #include "tinyxml2/tinyxml2.h"
+#include "taskManager/taskManager.h"
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -10,14 +11,14 @@
 	#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
 #endif
 
-using namespace std;
+//using namespace std;
 using namespace tinyxml2;
 
 int main(int argc, char *argv[]) {
 int  numtasks, rank, len, rc; 
 char hostname[MPI_MAX_PROCESSOR_NAME];
 double E0, Poisson0, a1, a2, a3, a4, C0, C1, alpha, Debug,fd0;     
-string inputFile;
+std::string inputFile;
 /*
 
 */       
@@ -34,26 +35,28 @@ MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 switch(rank) {      
     case 0:
         printf ("Started!\n");
-        ofstream fstrm;
-        ifstream finput;
+	std::ofstream fstrm;
+	std::ifstream finput;
         finput.open(argv[1]);
         finput >> E0 >> Poisson0 >> a1 >> a2 >> a3 >> a4;
         finput >> C0 >> C1 >> alpha >> Debug >> fd0;
         printf ("My rank= %d\n", rank);
-        cout << " E0= " << E0 <<" " << " fd0= " << fd0 << endl;  
+	std::cout << " E0= " << E0 <<" " << " fd0= " << fd0 << std::endl;  
         int inputI;
  	for (int i = 0; i < argc; i++) {
             if (strcmp("-i", argv[i]) == 0) {
 		  inputFile = argv[i + 1];  
 		  inputI = i;
 	    }  
-            cout << " argv[" << i << "] = " << argv[i] << endl;
+	    std::cout << " argv[" << i << "] = " << argv[i] << std::endl;
         }
-            cout << " inputFile = " << inputFile << endl;
+	std::cout << " inputFile = " << inputFile << std::endl;
 	XMLDocument xmlDoc;
 	XMLError eResult = xmlDoc.LoadFile(argv[inputI+1]);
         XMLCheckResult(eResult);
-        break;
+	taskManager tasks;// = new taskManager();
+        tasks.print();
+	break;
 }
 
 // this one is obvious  
---
+++ src/taskManager/taskManager.cpp
@@ -0,0 +1,21 @@
+#include "taskManager.h"
+
+void taskManager::print() {
+    std::cout << " print " << std::endl; 
+}
+
+void taskManager::initialize() {
+
+}
+
+void taskManager::assemble() {
+
+}
+
+void taskManager::run() {
+
+}
+
+void taskManager::postProcess() {
+
+}
